07e11a96ea3cd7757fec22246cc3bb3a64f9d308
encampeded <encampeded@gmail.com>
2025-01-08 10:58:04 -0500

    revised comments

--- example.py
+++ example.py
@@ -7,29 +7,27 @@ matrix = led_matrix.Matrix()
 matrix.set_matrix(0, 0)
 matrix.set_matrix(1, 0)
 
-# Turn off an LED by adding False
+# Set an LED back off by adding False
 matrix.set_matrix(0, 0, False)
 
-# Set the brightness 0-255
-matrix.send_brightness(32)
-
-matrix.send_matrix()
-matrix.reset()
-
 # draw_line() draws a line between two specified points
-# It can draw lines beyond vertical/horizontal, however the algorithm kinda sucks lol. I don't recommend using it
+# It *can* draw diagonal lines, however the algorithm kinda sucks. I recommend only using it for horizontal/vertical
 matrix.draw_line([1, 3], [1, 8])
 matrix.draw_line([3, 4], [7, 4])
 
 # draw_rectangle() draws a rectangle (duh)
-# First point is top left, with the second being bottom right, there's no error checking for this,
-# and what happens when you don't is undocumentede AKA I'm too lazy to check. Just do it the right way please.
+# First point is top left, with the second being bottom right
 matrix.draw_rectangle([2, 15], [7, 25])
 
 # Add True to make the rectangle filled
 matrix.draw_rectangle([1, 27], [7, 32], True)
 
+# Send the current array to the LED matrix
 matrix.send_matrix()
+matrix.reset()
+
+# Set the brightness of the matrix 0-255
+matrix.send_brightness(32)
 
 # Send patterns or other custom commands using send(command, [parameters])
 # Command Reference: https://github.com/FrameworkComputer/inputmodule-rs/blob/main/commands.md
--- led_matrix.py
+++ led_matrix.py
@@ -8,7 +8,7 @@ class Matrix:
     def reset(self):
         self.matrix = [False for i in range(312)]
 
-    # X=0-8, Y=0-33, (0, 0) is top left
+    # (0, 0) is top left
     def set_matrix(self, x, y, value=True):
         if not (0 <= x <= 8 and 0 <= y <= 33):
             raise Exception(f"Coordinates ({x}, {y}) out of range. X must be 0-8 and Y must be 0-33")
@@ -33,8 +33,7 @@ class Matrix:
                 self.set_matrix(i, point1[1])
             return
 
-        print("Sorry, this line drawing algo is janky")
-        print("if you want non-straight lines, i recommend doing it yourself w/ numpy or something. this sucks ass lol")
+        # If not straight, go into bad diagonal line drawer
         slope = (point2[1]-point1[1])/(point2[0]-point1[0])
 
         for i in range(point1[0], point2[0]+1):
@@ -42,7 +41,10 @@ class Matrix:
 
 
     def draw_rectangle(self, point1, point2, filled=False):
-        # Draw horizontal lines
+
+        if point1[0] > point2[0] or point1[1] > point2[1]:
+            raise Exception(f"point1 ({point1}) must be the higher and to the left of point2 ({point2})")
+
         for y in [point1[1], point2[1]]:
             self.draw_line([point1[0], y], [point2[0], y])
 
@@ -80,12 +82,12 @@ class Matrix:
             # Slice our matrix into the line of 8 and reverse bc int(x, 2) reads it reversed for some reason
             line = self.matrix[i:i+8][::-1]
 
-            # Copied from https://stackoverflow.com/a/68424066, convert out bool array to int, convert to integer
+            # Copied from https://stackoverflow.com/a/68424066, convert our bool array to int 0/1, convert to integer using binary
             line = int("".join(["01"[i] for i in line]), 2)
 
             matrix_encoded.append(line)
 
-            # Previous one-liner that i changed to make it a tad more readable
+            # Previous one-liner that I wrote out to make it a tad more readable
             # self.matrix_encoded.append(int("".join(["01"[i] for i in self.matrix[i:i+8]][::-1]), 2))
 
 
