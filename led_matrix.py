489feb1b7d505c5f8ab1a0d0fad2c41318f6bc30
encampeded <encampeded@gmail.com>
2025-03-12 11:53:24 -0400

    Added column send for per-led brightness, removed funky line drawing code

--- led_matrix.py
+++ led_matrix.py
@@ -1,60 +1,21 @@
-import serial
+import serial, time
 
 class Matrix:
 
-    # The working array that the user modifies through set_matrix
-    matrix = [False for i in range(312)]
+    matrix = [0 for i in range(312)]
+    default_brightness = 128
 
-    def reset(self):
-        self.matrix = [False for i in range(312)]
+    def reset(self, value = 0):
+        self.matrix = [value for i in range(312)]
 
-    # (0, 0) is top left
-    def set_matrix(self, x, y, value=True):
+    def set_matrix(self, x, y, brightness=default_brightness):
+
         if not (0 <= x <= 8 and 0 <= y <= 33):
             raise Exception(f"Coordinates ({x}, {y}) out of range. X must be 0-8 and Y must be 0-33")
+        if not (0 <= brightness <= 255):
+            raise Exception(f"Brightness {brightness} out of range. Brightness must be 0-255")
 
-        self.matrix[(y*9)+x] = value
-
-
-    def draw_line(self, point1, point2):
-        # Make point1 always to the left of point2
-        if point1[0] > point2[0]:
-            point1, point2 = point2, point1
-
-        # If X's are the same, draw vertical line
-        if point1[0] == point2[0]:
-            for i in range(point1[1], point2[1]+1, -1 if point1[1] > point2[1] else 1):
-                self.set_matrix(point1[0], i)
-            return
-
-        # If Y's are the same, draw horizontal line
-        if point1[1] == point2[1]:
-            for i in range(point1[0], point2[0]+1):
-                self.set_matrix(i, point1[1])
-            return
-
-        # If not straight, go into bad diagonal line drawer
-        slope = (point2[1]-point1[1])/(point2[0]-point1[0])
-
-        for i in range(point1[0], point2[0]+1):
-            self.set_matrix(i, round(point1[1]+(slope*(i-point1[0]))))
-
-
-    def draw_rectangle(self, point1, point2, filled=False):
-
-        if point1[0] > point2[0] or point1[1] > point2[1]:
-            raise Exception(f"point1 ({point1}) must be the higher and to the left of point2 ({point2})")
-
-        for y in [point1[1], point2[1]]:
-            self.draw_line([point1[0], y], [point2[0], y])
-
-        for x in [point1[0], point2[0]]:
-            self.draw_line([x, point1[1]+1], [x, point2[1]-1])
-
-        if filled:
-            for y in range(point1[1]+1, point2[1]):
-                self.draw_line([point1[0]+1, y], [point2[0]-1, y])
-
+        self.matrix[(y*9)+x] = brightness
 
     # Copied from framework example
     def send(self, command_id, parameters, with_response=False):
@@ -66,13 +27,22 @@ class Matrix:
                 res = s.read(32)
                 return res
 
-    def send_brightness(self, brightness):
-        if not (0 <= brightness <= 255):
-            raise Exception(f"Invalid brightness of {brightness}, must be 0-255.")
+    def csend(self):
+        columns = []
+        for x in range(9):
+            column = []
+            for y in range(x, 312, 9):
+                column.append(self.matrix[y])
+            columns.append(column)
 
-        self.send(0x00, [brightness])
+        for i in range(9):
+            self.send(0x07, [i, *columns[i]])
 
-    def send_matrix(self):
+        self.prev_columns = columns
+
+        self.send(0x08, [])
+
+    def qsend(self, brightness=default_brightness):
 
         # DrawBW (0x06) command takes 33 8-bit binary integers, each representing one line
         matrix_encoded = []
@@ -80,20 +50,14 @@ class Matrix:
         for i in range(0, 312, 8):
 
             # Slice our matrix into the line of 8 and reverse bc int(x, 2) reads it reversed for some reason
-            line = self.matrix[i:i+8][::-1]
+            line = [bool(i) for i in self.matrix[i:i+8][::-1]]
 
             # Copied from https://stackoverflow.com/a/68424066, convert our bool array to int 0/1, convert to integer using binary
             line = int("".join(["01"[i] for i in line]), 2)
 
             matrix_encoded.append(line)
 
-            # Previous one-liner that I wrote out to make it a tad more readable
-            # self.matrix_encoded.append(int("".join(["01"[i] for i in self.matrix[i:i+8]][::-1]), 2))
 
-
+        self.send(0x00, [brightness])
         self.send(0x06, matrix_encoded)
-
-
-
-
 
